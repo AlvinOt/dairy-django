37c653c7c940543ce7b8a5e2445ce37c75b93fa8
AlvinOt <thisisalvinhere@gmail.com>
2024-06-13 13:14:47 +0000

    create Farm model

--- dairy/models.py
+++ dairy/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Farm(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(
+        max_length=255, blank=True,
+        validators=[EmailValidator(message="Enter a valid email address.")]
+    )
+    phone = models.CharField(
+        max_length=20, blank=True,
+        validators=[RegexValidator(
+            regex=r'^\+?1?\d{9,15}$',
+            message="Enter a valid phone number."
+        )]
+    )
+    location = models.CharField(max_length=255)
+    description = models.TextField()
+    slogan = models.CharField(max_length=255, blank=True, null=True, help_text='Farm motto')
+
+
+    def __str__(self):
+        return self.name
