2f804116f89ae4192959362572d57986cf7e69aa
AlvinOt <thisisalvinhere@gmail.com>
2024-06-14 02:52:52 +0000

    customise models display

--- dairy/admin.py
+++ dairy/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Farm
 
+
 @admin.register(Farm)
 class FarmAdmin(admin.ModelAdmin):
     list_display = ['name', 'location', 'status', 'verified']
+    list_filter = ['status', 'created', 'verified']
+    search_fields = ['name', 'description']
+    prepopulated_fields = {'slug': ('name',)}
+    raw_id_fields = ['managers']
+    date_hierarchy = 'created'
+    ordering = ['status', 'verified']
